ca506a2fb3168a1b9aef2eea231bca5823a8c7e5
Michael Harper <Mike@microsoftlicensemoneypennyc.onmicrosoft.com>
2020-08-14 14:03:28 +0100

    update

--- main.js
+++ main.js
@@ -4,11 +4,28 @@ var app = new Vue({
     product: "Socks",
     description: "great",
     image: "vmSocks-green-onWhite.jpg",
-    inventory: 10,
+    inventory: 0,
     details: ["80% cotton", "20% polyester", "only men"],
+    cart: 0,
     variants: [
-      { variantId: 2234, variantColor: "green" },
-      { variantId: 1112, variantColor: "blue" },
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "vmSocks-green-onWhite.jpg",
+      },
+      {
+        variantId: 1112,
+        variantColor: "blue",
+        variantImage: "vmSocks-blue-onWhite.jpg",
+      },
     ],
+  },
+  methods: {
+    addToCart: function () {
+      this.cart++;
+    },
+    updateProduct: function (image) {
+      this.image = image;
+    },
   },
 });
