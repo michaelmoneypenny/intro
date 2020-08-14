1c9a39c90f11111aebfc72362cf533cc51dceb39
Michael Harper <Mike@microsoftlicensemoneypennyc.onmicrosoft.com>
2020-08-14 15:53:17 +0100

    upd

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
     description: "great",
-    image: "vmSocks-green-onWhite.jpg",
+    selectedVariant: 0,
     inventory: 0,
     details: ["80% cotton", "20% polyester", "only men"],
     cart: 0,
@@ -12,11 +13,13 @@ var app = new Vue({
         variantId: 2234,
         variantColor: "green",
         variantImage: "vmSocks-green-onWhite.jpg",
+        variantQuantity: 0,
       },
       {
         variantId: 1112,
         variantColor: "blue",
         variantImage: "vmSocks-blue-onWhite.jpg",
+        variantQuantity: 10,
       },
     ],
   },
@@ -24,8 +27,20 @@ var app = new Vue({
     addToCart: function () {
       this.cart++;
     },
-    updateProduct: function (image) {
-      this.image = image;
+    updateProduct: function (index) {
+      this.selectedVariant = index;
+    },
+  },
+  // computeds are cached so more performant if data may stay the same for a period of time
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
     },
   },
 });
